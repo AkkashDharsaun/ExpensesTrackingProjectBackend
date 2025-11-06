193535483d57dd6687833dd607d52dd7b42eb46c
Akkash Dharsaun <akkashdharsaun02@gmail.com>
2025-11-06 21:31:06 +0530

    Updated backend Flask API and requirements.txt

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from routes.expense_routes import expense_bp
 app = Flask(__name__)
 
 # ✅ Allow requests from your React app (localhost:5173)
-CORS(app, resources={r"/expense_bp/*": {"origins": "http://localhost:5173"}}, supports_credentials=True)
+CORS(app, resources={r"/expense_bp/*": {"origins": "https://expenses-tracking-project-j22y.vercel.app"}}, supports_credentials=True)
 
 # Register blueprint
 app.register_blueprint(expense_bp, url_prefix='/expense_bp')
